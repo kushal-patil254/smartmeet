ab4c3a49a9a356b4e1f3e9c3e8b7ffe49bc613cb
kushal-patil254 <kushalpatil1911@gmail.com-GitHub-email>
2026-08-25 01:36:09 -0700

    Connect Railway MySQL database

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Kush@12345",
-    database: "smartmeet_db"
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
     if (err) {
-    console.log("Database Connection Failed:", err.message);
-}
- else {
+        console.log("Database Connection Failed:", err.message);
+    } else {
         console.log("Database Connected Successfully");
     }
 });
