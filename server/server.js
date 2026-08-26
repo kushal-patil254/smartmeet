6796c1af7050ce6b92d0c91f3c2086757e865f1b
kushal-patil254 <kushalpatil1911@gmail.com-GitHub-email>
2026-08-26 00:01:58 -0700

    Fix frontend CORS

--- server/server.js
+++ server/server.js
@@ -17,7 +17,10 @@ const server = http.createServer(app);
 // ==========================================
 
 app.use(cors({
-    origin: true,
+    origin: [
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
