0382b18ab58f7fa70e27d4d6ada00a0f3c196e32
kushal-patil254 <kushalpatil1911@gmail.com-GitHub-email>
2026-08-26 22:07:07 -0700

    Fix GitHub Pages Railway connection

--- server/server.js
+++ server/server.js
@@ -17,10 +17,11 @@ const server = http.createServer(app);
 // ==========================================
 
 app.use(cors({
-    origin: [
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-    ],
+  origin: [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://kushal-patil254.github.io"
+],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
